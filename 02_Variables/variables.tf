29060c745c505012d771c6d86f947ed8c24b9afd
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-01-02 19:35:49 +0530

    use varibles to name your vpc"

---
+++ 02_Variables/variables.tf
@@ -0,0 +1,26 @@
+variable "vpcname" {
+  type = string
+  default = "myvpc"
+}
+
+variable "sshport" {
+  type = number
+  default = 22
+}
+
+variable "enabled" {
+  default = true
+}
+
+variable "mylist" {
+  type = list(string)
+  default = [ "value1","value2" ]
+}
+
+variable "mymap" {
+  type = map
+  default = {
+    key1 = "Value1"
+    key2 = "Value2"
+  }
+}
