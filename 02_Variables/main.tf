0a42f0566d37a06f0ae8d9c1e0e3196ba3280564
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-01-02 19:41:45 +0530

    Access map for var value

--- 02_Variables/main.tf
+++ 02_Variables/main.tf
@@ -5,6 +5,6 @@ provider "aws" {
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = var.mylist[1]  #Name = value2
+    Name = var.mymap["key1"]  #Name = value1
   }
 }
