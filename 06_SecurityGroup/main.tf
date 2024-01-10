bcd063e0d5f1da0fea223edafdf8bcc87a84270e
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-01-10 13:20:20 +0530

    link SG to EC2

--- 06_SecurityGroup/main.tf
+++ 06_SecurityGroup/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami = "ami-0005e0cfe09cc9050"
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.webtraffic.name]
 }
 
 resource "aws_security_group" "webtraffic" {
